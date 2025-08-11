618b3e509fbec161b90aa6de5125a1aa525d08c1
Sparsh Khandelwal <120552096+Sparshkh23@users.noreply.github.com>
2025-08-11 23:05:00 +0530

    Time: 34 ms (21.51%) | Memory: 93.1 MB (30.11%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] productQueries(int n, int[][] queries) {
-        List<Integer> powers = new ArrayList<>();
         int mod = 1_000_000_007;
+        List<Integer> powers = new ArrayList<>();
         int bit = 0;
         while (n > 0) {
             if ((n & 1) == 1) {
@@ -10,14 +10,36 @@ class Solution {
             n >>= 1;
             bit++;
         }
+        int[] prefixProduct = new int[powers.size()];
+        prefixProduct[0] = powers.get(0);
+        for (int i = 1; i < powers.size(); i++) {
+            prefixProduct[i] = (int)((long)prefixProduct[i - 1] * powers.get(i) % mod);
+        }
         int[] result = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int product = 1;
-            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                product = (int)((long)product * powers.get(j) % mod);
+            int start = queries[i][0];
+            int end = queries[i][1];
+            if (start == 0) {
+                result[i] = prefixProduct[end];
+            } else {
+                result[i] = (int)((long)prefixProduct[end] * modInverse(prefixProduct[start - 1], mod) % mod);
             }
-            result[i] = product;
         }
         return result;
+    }
+    private int modInverse(int a, int mod) {
+        return power(a, mod - 2, mod);
+    }
+    private int power(int a, int b, int mod) {
+        long result = 1;
+        long base = a;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            b >>= 1;
+        }
+        return (int) result;
     }
 }
